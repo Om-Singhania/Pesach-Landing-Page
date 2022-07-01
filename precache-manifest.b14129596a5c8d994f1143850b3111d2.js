9dcb557a9c80c4c0c48747564d00fddcfa9a9bb9
Omparkash12 <omparkash.op4023@gmail.com>
2022-07-01 16:24:34 +0530

    Updates

---
+++ precache-manifest.b14129596a5c8d994f1143850b3111d2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "039e6c6745ecedf6fe7d",
+    "url": "/Pesach-Landing-Page/static/css/main.467cc6d8.chunk.css"
+  },
+  {
+    "revision": "039e6c6745ecedf6fe7d",
+    "url": "/Pesach-Landing-Page/static/js/main.039e6c67.chunk.js"
+  },
+  {
+    "revision": "8f1f9bdd8571ce5e7bb7",
+    "url": "/Pesach-Landing-Page/static/js/1.8f1f9bdd.chunk.js"
+  },
+  {
+    "revision": "709c308c3bd5880b579b",
+    "url": "/Pesach-Landing-Page/static/css/2.0be17d88.chunk.css"
+  },
+  {
+    "revision": "709c308c3bd5880b579b",
+    "url": "/Pesach-Landing-Page/static/js/2.709c308c.chunk.js"
+  },
+  {
+    "revision": "91926eec1f13dd685077",
+    "url": "/Pesach-Landing-Page/static/js/runtime~main.91926eec.js"
+  },
+  {
+    "revision": "501e4d68aeb1c6b0eef75e22b8ac11f3",
+    "url": "/Pesach-Landing-Page/static/media/guitar1.501e4d68.png"
+  },
+  {
+    "revision": "cd3b611db1ddfc72894908f0516a48a7",
+    "url": "/Pesach-Landing-Page/static/media/guitar2.cd3b611d.png"
+  },
+  {
+    "revision": "d8d2030a80caa730c044589a4afefa85",
+    "url": "/Pesach-Landing-Page/static/media/guitar3.d8d2030a.png"
+  },
+  {
+    "revision": "2658e8562c3880c90454b91d63199fd2",
+    "url": "/Pesach-Landing-Page/static/media/guitar-img1.2658e856.png"
+  },
+  {
+    "revision": "3e721b4f2897497a8113d795d545c7b0",
+    "url": "/Pesach-Landing-Page/static/media/guitar-img2.3e721b4f.png"
+  },
+  {
+    "revision": "6ea03cd6dfc6faf6d83cf8ac1ad65b3a",
+    "url": "/Pesach-Landing-Page/static/media/guitar-img3.6ea03cd6.png"
+  },
+  {
+    "revision": "0855adfd834ba460cd6fdffbb8e7bf2d",
+    "url": "/Pesach-Landing-Page/static/media/guitar-img4.0855adfd.png"
+  },
+  {
+    "revision": "8d1fe83332a1abf873577c3199631c37",
+    "url": "/Pesach-Landing-Page/static/media/guitar-img5.8d1fe833.png"
+  },
+  {
+    "revision": "54f968bd6d1ee0ae9dcfba08785354ba",
+    "url": "/Pesach-Landing-Page/static/media/cardsection1.54f968bd.png"
+  },
+  {
+    "revision": "3eb6f950ab8b1d73e2226da7185a8e9c",
+    "url": "/Pesach-Landing-Page/static/media/cardsection2.3eb6f950.png"
+  },
+  {
+    "revision": "910c614eecd1f67f58a364d029ac97a4",
+    "url": "/Pesach-Landing-Page/static/media/cardsection3.910c614e.png"
+  },
+  {
+    "revision": "6a598aa1584b61e9a835707a40708d4a",
+    "url": "/Pesach-Landing-Page/static/media/dashboard-img.6a598aa1.png"
+  },
+  {
+    "revision": "ba22db780aaa6e0259cff636a3b90060",
+    "url": "/Pesach-Landing-Page/static/media/background2.ba22db78.jpg"
+  },
+  {
+    "revision": "b956e07c1a40b5c94fd0c982ce1c19fb",
+    "url": "/Pesach-Landing-Page/static/media/background1.b956e07c.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Pesach-Landing-Page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Pesach-Landing-Page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Pesach-Landing-Page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Pesach-Landing-Page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "43dcf61e71a20cdaffddece27bc629dd",
+    "url": "/Pesach-Landing-Page/static/media/card1.43dcf61e.png"
+  },
+  {
+    "revision": "d461d70f03e35495d623de6f85c794f4",
+    "url": "/Pesach-Landing-Page/static/media/card2.d461d70f.png"
+  },
+  {
+    "revision": "c785324cfdfcff5ab4e3a0aa03f719f0",
+    "url": "/Pesach-Landing-Page/static/media/card4.c785324c.png"
+  },
+  {
+    "revision": "37ba9ccc5c7675f27a615376e6a1bf47",
+    "url": "/Pesach-Landing-Page/static/media/card3.37ba9ccc.png"
+  },
+  {
+    "revision": "3536acb19fa00340f82890ee38df2045",
+    "url": "/Pesach-Landing-Page/static/media/card5.3536acb1.png"
+  },
+  {
+    "revision": "fa5e85a992cc4de726eea1db02999ac1",
+    "url": "/Pesach-Landing-Page/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Pesach-Landing-Page/precache-manifest.b14129596a5c8d994f1143850b3111d2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Pesach-Landing-Page/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
